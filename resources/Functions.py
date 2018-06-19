688f8b69ac44459983e2dbab515d797b911794fd
SemchenkoSergey <cem4enko.cepreu@gmail.com>
2018-06-19 16:51:33 +0300

    Добавил папку error для ошибок

--- resources/Functions.py
+++ resources/Functions.py
@@ -96,7 +96,7 @@ def connect_dslam(host):
         dslam = DslamHuawei.DslamHuawei5616(ip, Settings.login_5616, Settings.password_5616)
         
     dslam.program_profiles = {}
-    re_string = r'^auto_(\d+)-\d+-\d+$'
+    re_string = r'^petr_(\d+)-\d+-\d+$'
     for profile in dslam.adsl_line_profile:
         work_profile = re.search(re_string, dslam.adsl_line_profile[profile]['profile_name'])
         if work_profile:
@@ -123,31 +123,39 @@ def run(arguments):
     with open('profile_logs{}{} {}.txt'.format(os.sep, hostname, datetime.datetime.now().strftime('%d-%m-%y')), 'a') as log_file:
         log_file.write(' {} '.format(datetime.datetime.now().strftime('%d-%m-%y %H:%M')).center(100, '-'))
         log_file.write('\n')
+        
+        try:
 
-        for board in dslam.boards:
-            current_profiles = dslam.get_adsl_line_profile_board(board)
-            for port in range(0, dslam.ports):
-                key = '{}/{}/{}'.format(hostname, board, port)
-                if (key not in data) or (key in BLACK_LIST):
-                    continue
-                if data[key]['speed'] <= Settings.threshold_delta:
-                    delta = Settings.before_delta
-                else:
-                    delta = Settings.after_delta
-                profile_speed = choose_profile(delta, data[key]['speed'], data[key]['tariff'], data[key]['tv'])
-                valid_profile = [dslam.program_profiles[data[key]['speed'] + 1 - x] for x in range(0, delta + 1) if (data[key]['speed'] + 1 - x) in range(1,17)]
-                if dslam.program_profiles[profile_speed] == current_profiles[port]:
-                    log_file.write('{} - стоит оптимальный профиль (скорость {}, тариф {}, TV {}, профиль {})\n'.format(key, data[key]['speed'], data[key]['tariff'], data[key]['tv'], dslam.adsl_line_profile[dslam.program_profiles[profile_speed]]['profile_name']))
-                    continue
-                elif current_profiles[port] in valid_profile:
-                    log_file.write('{} - скорость не выходила за рамки профиля (скорость {}, тариф {}, TV {}, профиль {})\n'.format(key, data[key]['speed'], data[key]['tariff'], data[key]['tv'], dslam.adsl_line_profile[current_profiles[port]]['profile_name']))
-                    continue
-                if profile_speed in dslam.program_profiles:
-                    log_file.write('{} - скорость {}, тариф {}, TV {}, профиль {}\n'.format(key, data[key]['speed'], data[key]['tariff'], data[key]['tv'], dslam.adsl_line_profile[dslam.program_profiles[profile_speed]]['profile_name']))
-                    dslam.set_adsl_line_profile_port(board, port, dslam.program_profiles[profile_speed])
-                    time.sleep(1)
-                else:
-                    log_file.write('{} - не удалось найти профиль, скорость {}, тариф {}, TV {}\n'.format(key, data[key]['speed'], data[key]['tariff'], data[key]['tv']))
-                    continue
+            for board in dslam.boards:
+                current_profiles = dslam.get_adsl_line_profile_board(board)
+                for port in range(0, dslam.ports):
+                    key = '{}/{}/{}'.format(hostname, board, port)
+                    if (key not in data) or (key in BLACK_LIST):
+                        continue
+                    if data[key]['speed'] <= Settings.threshold_delta:
+                        delta = Settings.before_delta
+                    else:
+                        delta = Settings.after_delta
+                    profile_speed = choose_profile(delta, data[key]['speed'], data[key]['tariff'], data[key]['tv'])
+                    valid_profile = [dslam.program_profiles[data[key]['speed'] + 1 - x] for x in range(0, delta + 1) if (data[key]['speed'] + 1 - x) in range(1,17)]
+                    if dslam.program_profiles[profile_speed] == current_profiles[port]:
+                        log_file.write('{} - стоит оптимальный профиль (скорость {}, тариф {}, TV {}, профиль {})\n'.format(key, data[key]['speed'], data[key]['tariff'], data[key]['tv'], dslam.adsl_line_profile[dslam.program_profiles[profile_speed]]['profile_name']))
+                        continue
+                    elif current_profiles[port] in valid_profile:
+                        log_file.write('{} - скорость не выходила за рамки профиля (скорость {}, тариф {}, TV {}, профиль {})\n'.format(key, data[key]['speed'], data[key]['tariff'], data[key]['tv'], dslam.adsl_line_profile[current_profiles[port]]['profile_name']))
+                        continue
+                    if profile_speed in dslam.program_profiles:
+                        log_file.write('{} - скорость {}, тариф {}, TV {}, профиль {}\n'.format(key, data[key]['speed'], data[key]['tariff'], data[key]['tv'], dslam.adsl_line_profile[dslam.program_profiles[profile_speed]]['profile_name']))
+                        dslam.set_adsl_line_profile_port(board, port, dslam.program_profiles[profile_speed])
+                        time.sleep(1)
+                    else:
+                        log_file.write('{} - не удалось найти профиль, скорость {}, тариф {}, TV {}\n'.format(key, data[key]['speed'], data[key]['tariff'], data[key]['tv']))
+                        continue
+            except Exception as ex:
+                with open('errors{}{} {}.txt'.format(os.sep, hostname, datetime.datetime.now().strftime('%d-%m-%y')), 'a') as err_file:
+                    err_file.write(' {} '.format(datetime.datetime.now().strftime('%d-%m-%y %H:%M')).center(100, '-'))
+                    err_file.write('\n')
+                    err_file.write(ex)
+                return False
     print('{} обработан'.format(hostname))
     del dslam
