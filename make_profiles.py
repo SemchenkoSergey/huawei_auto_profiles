da1e5bfed85158ba784a5dad5a31db58b3009b8d
SemchenkoSergey <cem4enko.cepreu@gmail.com>
2018-06-20 11:31:41 +0300

    Изменил профили линий

--- make_profiles.py
+++ make_profiles.py
@@ -5,22 +5,22 @@ from resources import DslamHuawei
 from resources import Settings
 
 command_list = [
-    'adsl line-profile quickadd basic-para all trellis 1 bitswap 1 1 channel interleaved 6 6 adapt at-runtime snr 9 0 31 6 0 31 6 12 3 9 shifttime 10 10 10  10 rate 32 1024 32 920 name auto_1-1-9',
-    'adsl line-profile quickadd basic-para all trellis 1 bitswap 1 1 channel interleaved 6 6 adapt at-runtime snr 9 0 31 6 0 31 6 12 3 9 shifttime 10 10 10  10 rate 32 2048 32 920 name auto_2-1-9',
-    'adsl line-profile quickadd basic-para all trellis 1 bitswap 1 1 channel interleaved 6 6 adapt at-runtime snr 9 0 31 6 0 31 6 12 3 9 shifttime 10 10 10  10 rate 32 3072 32 920 name auto_3-1-9',
-    'adsl line-profile quickadd basic-para all trellis 1 bitswap 1 1 channel interleaved 6 6 adapt at-runtime snr 9 0 31 6 0 31 6 12 3 9 shifttime 10 10 10  10 rate 32 4096 32 920 name auto_4-1-9',
-    'adsl line-profile quickadd basic-para all trellis 1 bitswap 1 1 channel interleaved 6 6 adapt at-runtime snr 9 0 31 6 0 31 6 12 3 9 shifttime 10 10 10  10 rate 32 5120 32 920 name auto_5-1-9',
-    'adsl line-profile quickadd basic-para all trellis 1 bitswap 1 1 channel interleaved 6 6 adapt at-runtime snr 9 0 31 6 0 31 6 12 3 9 shifttime 10 10 10  10 rate 32 6144 32 920 name auto_6-1-9',
-    'adsl line-profile quickadd basic-para all trellis 1 bitswap 1 1 channel interleaved 6 6 adapt at-runtime snr 9 0 31 6 0 31 6 12 3 9 shifttime 10 10 10  10 rate 32 7168 32 920 name auto_7-1-9',
-    'adsl line-profile quickadd basic-para all trellis 1 bitswap 1 1 channel interleaved 6 6 adapt at-runtime snr 9 0 31 6 0 31 6 12 3 9 shifttime 10 10 10  10 rate 32 8192 32 920 name auto_8-1-9',
-    'adsl line-profile quickadd basic-para all trellis 1 bitswap 1 1 channel interleaved 6 6 adapt at-runtime snr 9 0 31 6 0 31 6 12 3 9 shifttime 10 10 10  10 rate 32 9216 32 920 name auto_9-1-9',
-    'adsl line-profile quickadd basic-para all trellis 1 bitswap 1 1 channel interleaved 6 6 adapt at-runtime snr 9 0 31 6 0 31 6 12 3 9 shifttime 10 10 10  10 rate 32 10240 32 920 name auto_10-1-9',
-    'adsl line-profile quickadd basic-para all trellis 1 bitswap 1 1 channel interleaved 6 6 adapt at-runtime snr 9 0 31 6 0 31 6 12 3 9 shifttime 10 10 10  10 rate 32 11264 32 920 name auto_11-1-9',
-    'adsl line-profile quickadd basic-para all trellis 1 bitswap 1 1 channel interleaved 6 6 adapt at-runtime snr 9 0 31 6 0 31 6 12 3 9 shifttime 10 10 10  10 rate 32 12288 32 920 name auto_12-1-9',
-    'adsl line-profile quickadd basic-para all trellis 1 bitswap 1 1 channel interleaved 6 6 adapt at-runtime snr 9 0 31 6 0 31 6 12 3 9 shifttime 10 10 10  10 rate 32 13312 32 920 name auto_13-1-9',
-    'adsl line-profile quickadd basic-para all trellis 1 bitswap 1 1 channel interleaved 6 6 adapt at-runtime snr 9 0 31 6 0 31 6 12 3 9 shifttime 10 10 10  10 rate 32 14336 32 920 name auto_14-1-9',
-    'adsl line-profile quickadd basic-para all trellis 1 bitswap 1 1 channel interleaved 6 6 adapt at-runtime snr 9 0 31 6 0 31 6 12 3 9 shifttime 10 10 10  10 rate 32 15360 32 920 name auto_15-1-9',
-    'adsl line-profile quickadd basic-para all trellis 1 bitswap 1 1 channel interleaved 6 6 adapt at-runtime snr 9 0 31 6 0 31 6 12 3 9 shifttime 10 10 10  10 rate 32 16384 32 920 name auto_16-1-9'
+    'adsl line-profile quickadd basic-para all trellis 1 bitswap 0 0 channel interleaved 6 6 adapt at-startup snr 10 0 31 8 0 31 rate 32 1024 32 896 name petr_1-1-10',
+    'adsl line-profile quickadd basic-para all trellis 1 bitswap 0 0 channel interleaved 6 6 adapt at-startup snr 10 0 31 8 0 31 rate 32 2048 32 896 name petr_2-1-10',
+    'adsl line-profile quickadd basic-para all trellis 1 bitswap 0 0 channel interleaved 6 6 adapt at-startup snr 10 0 31 8 0 31 rate 32 3072 32 896 name petr_3-1-10',
+    'adsl line-profile quickadd basic-para all trellis 1 bitswap 0 0 channel interleaved 6 6 adapt at-startup snr 10 0 31 8 0 31 rate 32 4096 32 896 name petr_4-1-10',
+    'adsl line-profile quickadd basic-para all trellis 1 bitswap 0 0 channel interleaved 6 6 adapt at-startup snr 10 0 31 8 0 31 rate 32 5120 32 896 name petr_5-1-10',
+    'adsl line-profile quickadd basic-para all trellis 1 bitswap 0 0 channel interleaved 6 6 adapt at-startup snr 10 0 31 8 0 31 rate 32 6144 32 896 name petr_6-1-10',
+    'adsl line-profile quickadd basic-para all trellis 1 bitswap 0 0 channel interleaved 6 6 adapt at-startup snr 10 0 31 8 0 31 rate 32 7168 32 896 name petr_7-1-10',
+    'adsl line-profile quickadd basic-para all trellis 1 bitswap 0 0 channel interleaved 6 6 adapt at-startup snr 10 0 31 8 0 31 rate 32 8192 32 896 name petr_8-1-10',
+    'adsl line-profile quickadd basic-para all trellis 1 bitswap 0 0 channel interleaved 6 6 adapt at-startup snr 10 0 31 8 0 31 rate 32 9216 32 896 name petr_9-1-10',
+    'adsl line-profile quickadd basic-para all trellis 1 bitswap 0 0 channel interleaved 6 6 adapt at-startup snr 10 0 31 8 0 31 rate 32 10240 32 896 name petr_10-1-10',
+    'adsl line-profile quickadd basic-para all trellis 1 bitswap 0 0 channel interleaved 6 6 adapt at-startup snr 10 0 31 8 0 31 rate 32 11264 32 896 name petr_11-1-10',
+    'adsl line-profile quickadd basic-para all trellis 1 bitswap 0 0 channel interleaved 6 6 adapt at-startup snr 10 0 31 8 0 31 rate 32 12288 32 896 name petr_12-1-10',
+    'adsl line-profile quickadd basic-para all trellis 1 bitswap 0 0 channel interleaved 6 6 adapt at-startup snr 10 0 31 8 0 31 rate 32 13312 32 896 name petr_13-1-10',
+    'adsl line-profile quickadd basic-para all trellis 1 bitswap 0 0 channel interleaved 6 6 adapt at-startup snr 10 0 31 8 0 31 rate 32 14336 32 896 name petr_14-1-10',
+    'adsl line-profile quickadd basic-para all trellis 1 bitswap 0 0 channel interleaved 6 6 adapt at-startup snr 10 0 31 8 0 31 rate 32 15360 32 896 name petr_15-1-10',
+    'adsl line-profile quickadd basic-para all trellis 1 bitswap 0 0 channel interleaved 6 6 adapt at-startup snr 10 0 31 8 0 31 rate 32 16384 32 896 name petr_16-1-10'
 ]
 
 for host in Settings.hosts:
